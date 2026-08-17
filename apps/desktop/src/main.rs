decfbfdf3149f3899b96813890ae375d32e0798b
Gersi365 <Gersi365@outlook.com>
2026-08-17 06:18:48 +0200

    phase 151: add desktop application entrypoint

---
+++ apps/desktop/src/main.rs
@@ -0,0 +1,14 @@
+mod ipc;
+mod state;
+mod ui;
+
+use adw::prelude::*;
+
+fn main() -> gtk::glib::ExitCode {
+    let application = adw::Application::builder()
+        .application_id("io.patchmirror.prw.desktop")
+        .build();
+
+    application.connect_activate(ui::build);
+    application.run()
+}
