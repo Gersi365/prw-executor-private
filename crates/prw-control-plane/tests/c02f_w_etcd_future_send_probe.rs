70fe00b5a102829cc908626c522598eb776ec5a0
powercode365-dotcom <powercode365@gmail.com>
2026-08-20 06:43:01 +0200

    probe c02f-w: apply final rustfmt layout

--- crates/prw-control-plane/tests/c02f_w_etcd_future_send_probe.rs
+++ crates/prw-control-plane/tests/c02f_w_etcd_future_send_probe.rs
@@ -3,16 +3,12 @@ use std::future::Future;
 use etcd_client::{Client, Error, GetResponse, Txn, TxnResponse};
 
 #[allow(dead_code)]
-fn get_future(
-    client: &mut Client,
-) -> impl Future<Output = Result<GetResponse, Error>> + Send + '_ {
+fn get_future(client: &mut Client) -> impl Future<Output = Result<GetResponse, Error>> + Send + '_ {
     client.get(b"prw-c02f-w-probe".to_vec(), None)
 }
 
 #[allow(dead_code)]
-fn txn_future(
-    client: &mut Client,
-) -> impl Future<Output = Result<TxnResponse, Error>> + Send + '_ {
+fn txn_future(client: &mut Client) -> impl Future<Output = Result<TxnResponse, Error>> + Send + '_ {
     client.txn(Txn::new())
 }
 
