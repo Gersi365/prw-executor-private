a20e54f8952879863b60fa1d9c6a270aa39e2715
Gersi365 <Gersi365@outlook.com>
2026-09-10 20:28:06 +0200

    C03e-PJ: apply rustfmt layout

--- crates/prw-agent/src/remote_session_capability_runtime/remote_session_executor_runtime.rs
+++ crates/prw-agent/src/remote_session_capability_runtime/remote_session_executor_runtime.rs
@@ -2168,7 +2168,7 @@ impl RemoteSessionExecutorRuntime {
     ) -> Result<
         super::authenticated_remote_session_runtime::AuthenticatedRemoteSessionFallibleVerifierTimeWorkerStop,
         RemoteSessionSpawnedWorkerJoinError,
-    > {
+    >{
         let authority = (*authority).clone();
 
         self.runtime.block_on(async move {
